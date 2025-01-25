155c77f84d42dae653cb9eaa2789ba01ccd2f848
lovelysehotra <lsehotra12@gmail.com>
2025-01-26 01:23:06 +0530

    add storybook in react

---
+++ hr-client/src/components/Input/Input.stories.js
@@ -0,0 +1,16 @@
+import Input from "./Input";
+
+
+export default {
+  component: Input,
+};
+
+export const main = {
+  args: {
+    label: true,
+    labelText: "Name",
+    id: "",
+    type: "text",
+    placeholder: "input"
+  },
+};
