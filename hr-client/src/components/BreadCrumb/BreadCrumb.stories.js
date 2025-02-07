0867e2a13b5370bce8ca2c66ec0266c843491bbd
lovelysehotra <lsehotra12@gmail.com>
2025-02-07 22:27:40 +0530

    setup breadcrumb

---
+++ hr-client/src/components/BreadCrumb/BreadCrumb.stories.js
@@ -0,0 +1,12 @@
+import Breadcrumbs from "./BreadCrumb.jsx";
+
+
+export default {
+  component: Breadcrumbs,
+};
+
+export const main = {
+  args: {
+    sender:"lovely", message:"HEllo,here is my ", timestamp:"2 oct"
+  },
+};
