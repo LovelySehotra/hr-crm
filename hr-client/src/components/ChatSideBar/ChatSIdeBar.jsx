9e4f37f816dc2d84d34afcc819b7cdead068774f
lovelysehotra <lsehotra12@gmail.com>
2025-02-11 00:27:26 +0530

    add chat side bar

--- hr-client/src/components/ChatSideBar/ChatSIdeBar.jsx
+++ hr-client/src/components/ChatSideBar/ChatSIdeBar.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
+import Chat from "../../assets/Chat/Chat.png";
+import Status from "../../assets/Chat/Status.png";
+import Channel from "../../assets/Chat/Channel.png";
+import Profile from "../../assets/TopBar/Profile.png"
+import "./ChatSIdeBar.css"
 const ChatSIdeBar = () => {
   return (
-    <div>
-      hello
-    </div>
+    <main className='main_box'>
+      <div className="chat_sidebar">
+        <div className="chat_sidebar_top">
+          <div className='box_icon'> 
+            <Link to="/home"><img src={Chat} alt="" /></Link>
+          </div>
+          <div className='box_icon '>
+
+            <Link to="/home"><img src={Status} alt="" /></Link>
+          </div>
+          <div className='box_icon '>
+
+          <Link to="/home"><img src={Channel} alt="" /></Link>
+          </div>
+        </div>
+        <div className='chat_sidebar_bottom'>
+          <div className='box_icon '>
+
+          <Link to="/home"><img src={Profile} alt="" /></Link>
+          </div>
+          <div className='box_icon '> 
+
+          <Link to="/home"><img src={Channel} alt="" /></Link>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
